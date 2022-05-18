9932451a2ee36fd94640dbe4a31d1e95a2c780ba
manka543 <manka543@gmail.com>
2022-05-18 20:55:06 +0200

    Dodanie kometarzy i todo

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import pygame
-from random import randint
 import snake
 
-pygame.init()
+pygame.init() # Inicjalizacja pygama
 
 # Game variables
-gameSpeed = 1000 # This int change game speed
-gameClock = gameSpeed
-font = pygame.font.Font('freesansbold.ttf', 72)
+font = pygame.font.Font('freesansbold.ttf', 72) #TODO: Przenieś do klasy snake i to niżej też
 endstr = font.render("Koniec, Przegrałeś!!!", True, (255,255,255))
 
 
 screen = pygame.display.set_mode((900,600)) # Board 36x24[squares]; Square 25x25[px]
 
+# TODO: Po dodaniu możliwości zginięcia dodaj to do klasy snake
 def end():
     screen.blit(endstr, (400, 250))
 
-snake = snake.Snake(screen)
+snake = snake.Snake(screen) # Twożenie objektu klasy snake
 
 
+# TODO: To wsumie też można zrobić klase game i tam przenieść prawie wszystko żeby jak ktoś to będzie przeglądać to nic nie bedzie wiedzieć
 running = True
 while running:
     for event in pygame.event.get():
--- snake.py
+++ snake.py
@@ -1,16 +1,21 @@
-from nis import match
-from operator import truediv
 import pygame
 from random import randint
 
+# TODO: Wszystko związane z jabłkiem
+# TODO: Poprawić responsywność poruszania
+# TODO: Kolizje węża ze ścianami i samym sobą; Część dodana ale powodowała błędy i jest aktualnie wyłączona
 
+# Klasa snake jest utwożona z oparciem o bibliotekę pygame i random
+# Pozwala ona na stwożenie i obsługę węża z wykożystaniem kilku funkcji
 class Snake:
     # Variables
-    body = []
+    body = [] # Twoży listę żeby można było do niej dodać dane w formanie "tuple"; Bez tego do body dodawały się zmienne nie opakowane w tuple co nie jest zgodne z założeniem zmiennej
     # Constructor
-    def __init__(self,surface, head = (18,12), lenght = 5, direction = "r",directionn = "r",directionc=False, directuoncc = False, speed = 100000):
+    def __init__(self,surface, head = (18,12), lenght = 5, direction = "r", speed = 100000):
+        
+        # Używając zmiennej head i lenght generuje ciało węża w lini prostej zgodnej z kierunkiem podanym w direction
 
-        if direction == "r":
+        if direction == "r": 
             for i in range(lenght):
                 self.body.append((head[0]-i,head[1]))
         elif direction == "l":
@@ -22,26 +27,26 @@ class Snake:
         elif direction == "d":
             for i in range(lenght):
                 self.body.append((head[0],head[1]+i))
-        self.speed = speed
-        self.surface = surface
-        self.time = self.speed
-        self.direction = direction
-        self.moves = []
+        self.speed = speed # Szybkość kaltki
+        self.surface = surface # Okno w którym działa wąż
+        self.time = self.speed # Czas ruchu
+        self.direction = direction # Kierunek poruszania się węża Legenda: 'l' - lewo, 'r' - prawo, 'u' - góra, 'd' - dół
+        self.moves = []  # Lista z następnymi ruchami
 
-    def printbody(self):
+    def printbody(self): # Pomocnicza funkcja w debugowaniu ktróra wypisuje części ciała węża
         print(type(self.body[1]))
 
-    def drawBody(self):
+    def drawBody(self): # Ta rysuje węża w okienku
         for i in self.body:
             pygame.draw.rect(self.surface,(0,255,0),(i[0]*25,i[1]*25,25,25))
 
-    def ismove(self):
+    def ismove(self): # Taki zegarek który wywołuje ruch co zmienną speed przy pomocy zmiennej time
         self.time -= 1
         if self.time == 0:
             self.time = self.speed
             return True
 
-    def move(self):
+    def move(self): # Ta funkcja wykonuje ruch
         if len(self.moves)>0:
             self.direction = self.moves.pop(0)
         self.movepredictf()
@@ -50,7 +55,7 @@ class Snake:
         for i in range(len(self.body)-1):
             self.body[len(self.body)-1-i]= self.body[len(self.body)-i-2]
 
-    def ismovepossible(self):
+    def ismovepossible(self): # Ta funkcja sprawdza czy następny ruch jest legalny
         if not self.movepredict in self.body:   
             if self.direction == 'r' and self.movepredict[0]+1 > 36:
                 return True
@@ -64,7 +69,8 @@ class Snake:
                 return False
         else:
             return False
-    def movepredictf(self):
+
+    def movepredictf(self): # Tu sie dzieje przewidywanie ruchu i ustawianie go
         if self.direction == 'r':
             self.movepredict = (self.body[0][0]+1,self.body[0][1])
         elif self.direction == 'l':
@@ -74,10 +80,10 @@ class Snake:
         elif self.direction == 'd':
             self.movepredict = (self.body[0][0],self.body[0][1]+1)
 
-    def eatapple(self):
+    def eatapple(self): # Ta funkcja obsługuje jedzenie jabłek tako
         pass
 
-    def setdirection(self,key):
+    def setdirection(self,key): # To jest funkcja która robi tablice następnych ruchów
         if key == pygame.K_UP and len(self.moves) == 0 and not self.direction == 'd':
             self.moves += 'u'
         elif key == pygame.K_UP and len(self.moves) > 0:
@@ -98,4 +104,3 @@ class Snake:
         elif key == pygame.K_LEFT and len(self.moves) > 0:
             if self.moves[len(self.moves) -1] == 'r':
                 self.moves += 'l'
-    
